e2b4f3d5bda783db6556874d1ee25998e8290498
rambertm <58461570+rambertm@users.noreply.github.com>
2025-04-23 16:52:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function popImg(){
   document.getElementById("duck").style.display="block";
 }
+function hideImg(){
+  let imgs=document.getElementsByClassName("popImg");
+  for(let i=0;i<imgs.length;i++){
+    imgs[i].style.display="none";
+  }
+}
